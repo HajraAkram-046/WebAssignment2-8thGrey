77427f4b3f41fa151cdb74374f7f8f13d8a7454d
HAJRA <HAJRA@DESKTOP-I71V0MD>
2023-05-16 23:03:37 +0500

    Add project files.

---
+++ Assignment2Web/Server/Controllers/portfolioController.cs
@@ -0,0 +1,45 @@
+﻿using Assignment2Web.Server.DataContext;
+using Assignment2Web.Shared;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Assignment2Web.Server.Controllers
+{
+    [Route("/[controller]")]
+    [ApiController]
+    public class PortfolioDetailsController : ControllerBase
+    {
+
+
+        // GET api/<PortfolioDetailsController>/5
+        [HttpGet("/Portfolio")]
+        public IActionResult Get()
+        {
+            var options = new DbContextOptionsBuilder<PortfolioContext>().UseSqlServer("Data Source=DESKTOP-I71V0MD;Initial Catalog=PortfolioDetails;integrated security=true;TrustServerCertificate=True").Options;
+            var PortfolioDB = new PortfolioContext(options);
+            var get_records = PortfolioDB.portfolioDetails.ToList();
+            return Ok(get_records);
+        }
+
+
+
+        // PUT api/<PortfolioDetailsController>/5
+        [HttpPut("/{name}/{description}/{url}")]
+        public IActionResult Put(string name, string description, string url)
+        {
+            var options = new DbContextOptionsBuilder<PortfolioContext>().UseSqlServer("Data Source=DESKTOP-I71V0MD;Initial Catalog=PortfolioDetails;integrated security=true;TrustServerCertificate=True").Options;
+            var PortfolioDB = new PortfolioContext(options);
+            var insert = new portfolioDetails
+            {
+                Name = name,
+                Description = description,
+                ImageUrl = url
+            };
+            PortfolioDB.portfolioDetails.Add(insert);
+            PortfolioDB.SaveChanges();
+            return Ok(insert);
+        }
+    }
+}
---
+++ Assignment2Web/Server/DataContext/PortfolioContext.cs
@@ -0,0 +1,15 @@
+﻿using Assignment2Web.Shared;
+using Microsoft.EntityFrameworkCore;
+
+namespace Assignment2Web.Server.DataContext
+{
+    public class PortfolioContext : DbContext
+    {
+        public PortfolioContext(DbContextOptions<PortfolioContext> options) : base(options)
+        {
+
+        }
+        public DbSet<portfolioDetails> portfolioDetails { get; set; }
+
+    }
+}
